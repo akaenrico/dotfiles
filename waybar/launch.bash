29ed4a817ab3c54384630cd6789e6d8c689f031e
Enrico B. Palma <enricob.palma@gmail.com>
2025-05-01 00:04:32 -0300

    Refactors waybar script and started hyprland.conf

--- waybar/launch.bash
+++ waybar/launch.bash
@@ -1,23 +1,53 @@
 #!/bin/bash
-
-# by Enrico "akaEnrico" Palma (2025)
+#
+#
+#
+# __    __ ____ __  _______   ____  _____
+# \ \/\/ // () \\ \/ /| () ) / () \ | () )
+#  \_/\_//__/\__\|__| |_()_)/__/\__\|_|\_\
+#
+#
+#
+# by Enrico "akaEnrico" Palma
 
 # Checks machine hostname
 HOSTNAME=$(uname -n)
 
 # Quit existing waybar instances
-# killall waybar
+killall waybar
 
-# Loads waybar configuration based on hostname
-if [[ "$HOSTNAME" == "fortuna" ]]; then
-  waybar -c ~/dotfiles/waybar/fortuna_config -s ~/dotfiles/waybar/style.css &
-  echo "Waybar configs loaded for fortuna"
-
-elif [[ "$HOSTNAME" == "leviathan" ]]; then
-  waybar -c ~/dotfiles/waybar/leviathan_config -s ~/dotfiles/waybar/style.css &
-  echo "Waybar configs loaded for leviathan"
-
-else
+# Sets config path based on hostname
+case "$HOSTNAME" in
+"fortuna")
+  CONFIG=~/dotfiles/waybar/fortuna_config
+  ;;
+"leviathan")
+  CONFIG=~/dotfiles/waybar/leviathan_config
+  ;;
+*)
+  echo "Unknown hostname: $HOSTNAME"
+  echo "Falling back to default waybar config."
   waybar &
-  echo "No available config for $HOSTNAME, rendering default configs"
+  exit 0
+  ;;
+esac
+
+# Style path
+STYLE=~/dotfiles/waybar/style.css
+
+# Checks if config file exists
+if [[ -f "$CONFIG" ]]; then
+  # Checks if style.css exists
+  if [[ -f "$STYLE" ]]; then
+    waybar -c "$CONFIG" -s "$STYLE" &
+    echo "Waybar configs loaded for $HOSTNAME with styles"
+  else
+    waybar -c "$CONFIG" &
+    echo "Waybar configs loaded for $HOSTNAME, BUT styles were not found"
+  fi
+else
+  echo "Expected configs for $HOSTNAME, not found at:"
+  echo "   $CONFIG"
+  echo "Falling back to default configs"
+  waybar &
 fi
